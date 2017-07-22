db66976ab369e17c75bd6980e4a3405f007b1379
Paul Campbell <pcampbell@kemitix.net>
2017-07-22 08:48:21 +0100

    Add logging

--- coveralls.sh
+++ coveralls.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+echo "Publishing coverage"
 if test -z ${COVERAGE_PROJECTS}
 then
     PROJECTS=""
+    echo " Projects: all"
 else
     PROJECTS="-pl ${COVERAGE_PROJECTS}"
+    echo " Projects: $COVERAGE_PROJECTS"
 fi
 
 ./mvnw ${PROJECTS} test jacoco:report coveralls:report
--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
+echo "Decrypting signing key"
 eval KEY=\$encrypted_${SIGN_KEY}_key
+echo " Key: $KEY"
 eval IV=\$encrypted_${SIGN_KEY}_iv
+echo " IV: $IV"
 openssl aes-256-cbc -K ${KEY} -iv ${IV} -in codesigning.asc.enc -out codesigning.asc -d
 
+echo "Loading signing key"
 gpg --batch --fast-import codesigning.asc
 
 if test -z ${DEPLOY_PROJECTS}
 then
     PROJECTS=""
+    echo "Deploying Projects: all"
 else
     PROJECTS="-pl ${DEPLOY_PROJECTS}"
+    echo "Deploying Projects: $DEPLOY_PROJECTS"
 fi
 
 ./mvnw ${PROJECTS} --settings .travis-support/settings.xml -Dskip-Tests=true -P release -B deploy
